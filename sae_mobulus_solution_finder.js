b35790136b567bbd517eaeef75da7638dd62839e
tjking20 <tjking20@outlook.com>
2018-07-12 11:37:42 -0400

    adding working version of solutioin finder logic

---
+++ sae_mobulus_solution_finder.js
@@ -0,0 +1,222 @@
+var data = '{"Answer01":{"A":{"S1":25,"S2":0,"S3":0,"S4":0,"S5":0,"S6":0,"S7":25,"S8":0,"S9":0,"S10":0,"S11":0,"S12":25,"S13":25,"S14":25,"S15":0,"S16":0,"S17":25,"S18":25,"S19":25,"S20":0,"S21":25,"S22":25,"S23":25,"S24":25,"S25":25},"B":{"S1":0,"S2":25,"S3":25,"S4":25,"S5":25,"S6":25,"S7":25,"S8":25,"S9":25,"S10":25,"S11":25,"S12":25,"S13":0,"S14":25,"S15":25,"S16":25,"S17":0,"S18":0,"S19":25,"S20":25,"S21":0,"S22":0,"S23":25,"S24":0,"S25":25},"C":{"S1":0,"S2":25,"S3":25,"S4":25,"S5":25,"S6":25,"S7":25,"S8":25,"S9":25,"S10":25,"S11":25,"S12":25,"S13":0,"S14":25,"S15":25,"S16":25,"S17":0,"S18":0,"S19":25,"S20":25,"S21":0,"S22":0,"S23":25,"S24":0,"S25":25},"D":{"S1":0,"S2":0,"S3":0,"S4":0,"S5":0,"S6":0,"S7":0,"S8":0,"S9":0,"S10":0,"S11":0,"S12":0,"S13":0,"S14":0,"S15":0,"S16":0,"S17":0,"S18":0,"S19":0,"S20":0,"S21":0,"S22":0,"S23":0,"S24":0,"S25":0},"Z":{"S1":0,"S2":0,"S3":0,"S4":0,"S5":0,"S6":0,"S7":0,"S8":0,"S9":0,"S10":0,"S11":0,"S12":0,"S13":0,"S14":0,"S15":0,"S16":0,"S17":0,"S18":0,"S19":0,"S20":0,"S21":0,"S22":0,"S23":0,"S24":0,"S25":0}},"Answer02":{"A":{"S1":0,"S2":50,"S3":50,"S4":0,"S5":0,"S6":50,"S7":50,"S8":50,"S9":0,"S10":0,"S11":0,"S12":50,"S13":0,"S14":50,"S15":0,"S16":0,"S17":0,"S18":0,"S19":50,"S20":0,"S21":0,"S22":0,"S23":0,"S24":0,"S25":50},"B":{"S1":0,"S2":50,"S3":50,"S4":0,"S5":0,"S6":50,"S7":50,"S8":50,"S9":0,"S10":0,"S11":0,"S12":50,"S13":0,"S14":50,"S15":0,"S16":0,"S17":0,"S18":0,"S19":50,"S20":0,"S21":0,"S22":0,"S23":0,"S24":0,"S25":50},"C":{"S1":0,"S2":0,"S3":0,"S4":50,"S5":50,"S6":0,"S7":50,"S8":0,"S9":50,"S10":50,"S11":50,"S12":50,"S13":0,"S14":50,"S15":50,"S16":50,"S17":0,"S18":0,"S19":50,"S20":50,"S21":0,"S22":0,"S23":50,"S24":0,"S25":50},"D":{"S1":0,"S2":0,"S3":0,"S4":50,"S5":50,"S6":0,"S7":50,"S8":0,"S9":50,"S10":50,"S11":50,"S12":50,"S13":0,"S14":50,"S15":50,"S16":50,"S17":0,"S18":0,"S19":50,"S20":50,"S21":0,"S22":0,"S23":50,"S24":0,"S25":50},"Z":{"S1":0,"S2":0,"S3":0,"S4":0,"S5":0,"S6":0,"S7":0,"S8":0,"S9":0,"S10":0,"S11":0,"S12":0,"S13":0,"S14":0,"S15":0,"S16":0,"S17":0,"S18":0,"S19":0,"S20":0,"S21":0,"S22":0,"S23":0,"S24":0,"S25":0}},"Answer03":{"A":{"S1":50,"S2":0,"S3":0,"S4":0,"S5":0,"S6":0,"S7":50,"S8":0,"S9":0,"S10":0,"S11":0,"S12":50,"S13":50,"S14":50,"S15":0,"S16":0,"S17":50,"S18":0,"S19":50,"S20":0,"S21":50,"S22":50,"S23":0,"S24":50,"S25":50},"B":{"S1":50,"S2":0,"S3":0,"S4":0,"S5":0,"S6":0,"S7":50,"S8":0,"S9":0,"S10":0,"S11":0,"S12":50,"S13":50,"S14":50,"S15":0,"S16":0,"S17":50,"S18":0,"S19":50,"S20":0,"S21":50,"S22":50,"S23":0,"S24":50,"S25":50},"C":{"S1":50,"S2":0,"S3":0,"S4":0,"S5":0,"S6":0,"S7":50,"S8":0,"S9":0,"S10":0,"S11":0,"S12":50,"S13":50,"S14":50,"S15":0,"S16":0,"S17":50,"S18":50,"S19":50,"S20":0,"S21":50,"S22":0,"S23":50,"S24":50,"S25":50},"Z":{"S1":0,"S2":0,"S3":0,"S4":0,"S5":0,"S6":0,"S7":0,"S8":0,"S9":0,"S10":0,"S11":0,"S12":0,"S13":0,"S14":0,"S15":0,"S16":0,"S17":0,"S18":0,"S19":0,"S20":0,"S21":0,"S22":0,"S23":0,"S24":0,"S25":0}},"Answer04":{"A":{"S1":100,"S2":100,"S3":100,"S4":100,"S5":0,"S6":0,"S7":0,"S8":0,"S9":100,"S10":100,"S11":100,"S12":100,"S13":100,"S14":100,"S15":100,"S16":0,"S17":100,"S18":0,"S19":100,"S20":100,"S21":100,"S22":100,"S23":100,"S24":100,"S25":100},"B":{"S1":100,"S2":0,"S3":100,"S4":0,"S5":100,"S6":100,"S7":100,"S8":100,"S9":0,"S10":100,"S11":0,"S12":0,"S13":100,"S14":100,"S15":0,"S16":100,"S17":100,"S18":100,"S19":100,"S20":0,"S21":100,"S22":100,"S23":100,"S24":100,"S25":0},"C":{"S1":100,"S2":0,"S3":100,"S4":0,"S5":100,"S6":100,"S7":100,"S8":100,"S9":0,"S10":100,"S11":0,"S12":0,"S13":100,"S14":100,"S15":0,"S16":100,"S17":100,"S18":100,"S19":100,"S20":0,"S21":100,"S22":100,"S23":100,"S24":100,"S25":0},"D":{"S1":100,"S2":100,"S3":100,"S4":100,"S5":100,"S6":100,"S7":100,"S8":100,"S9":100,"S10":100,"S11":100,"S12":100,"S13":100,"S14":100,"S15":100,"S16":100,"S17":100,"S18":100,"S19":100,"S20":100,"S21":100,"S22":100,"S23":100,"S24":100,"S25":100},"E":{"S1":0,"S2":0,"S3":0,"S4":0,"S5":0,"S6":0,"S7":0,"S8":0,"S9":0,"S10":0,"S11":0,"S12":0,"S13":0,"S14":0,"S15":0,"S16":0,"S17":0,"S18":0,"S19":0,"S20":0,"S21":0,"S22":0,"S23":0,"S24":0,"S25":0},"Z":{"S1":0,"S2":0,"S3":0,"S4":0,"S5":0,"S6":0,"S7":0,"S8":0,"S9":0,"S10":0,"S11":0,"S12":0,"S13":0,"S14":0,"S15":0,"S16":0,"S17":0,"S18":0,"S19":0,"S20":0,"S21":0,"S22":0,"S23":0,"S24":0,"S25":0}},"Answer05":{"A":{"S1":0,"S2":75,"S3":0,"S4":75,"S5":75,"S6":75,"S7":0,"S8":75,"S9":75,"S10":75,"S11":75,"S12":0,"S13":0,"S14":0,"S15":75,"S16":75,"S17":0,"S18":75,"S19":0,"S20":75,"S21":0,"S22":0,"S23":75,"S24":75,"S25":0},"B":{"S1":75,"S2":0,"S3":75,"S4":0,"S5":0,"S6":0,"S7":0,"S8":0,"S9":0,"S10":0,"S11":0,"S12":0,"S13":0,"S14":0,"S15":0,"S16":0,"S17":75,"S18":0,"S19":0,"S20":0,"S21":0,"S22":0,"S23":75,"S24":0,"S25":75},"C":{"S1":0,"S2":0,"S3":0,"S4":0,"S5":0,"S6":0,"S7":0,"S8":0,"S9":0,"S10":0,"S11":0,"S12":0,"S13":0,"S14":75,"S15":0,"S16":0,"S17":0,"S18":0,"S19":75,"S20":0,"S21":0,"S22":0,"S23":75,"S24":0,"S25":0},"D":{"S1":0,"S2":0,"S3":0,"S4":0,"S5":0,"S6":0,"S7":75,"S8":0,"S9":0,"S10":0,"S11":0,"S12":75,"S13":0,"S14":0,"S15":0,"S16":0,"S17":0,"S18":0,"S19":0,"S20":0,"S21":0,"S22":0,"S23":75,"S24":0,"S25":0},"E":{"S1":0,"S2":0,"S3":0,"S4":0,"S5":0,"S6":0,"S7":0,"S8":0,"S9":0,"S10":0,"S11":0,"S12":0,"S13":75,"S14":0,"S15":0,"S16":0,"S17":0,"S18":0,"S19":0,"S20":0,"S21":75,"S22":75,"S23":75,"S24":0,"S25":0},"F":{"S1":0,"S2":0,"S3":0,"S4":0,"S5":0,"S6":0,"S7":0,"S8":0,"S9":0,"S10":0,"S11":0,"S12":0,"S13":0,"S14":0,"S15":0,"S16":0,"S17":0,"S18":0,"S19":0,"S20":0,"S21":0,"S22":0,"S23":0,"S24":0,"S25":0},"Z":{"S1":0,"S2":0,"S3":0,"S4":0,"S5":0,"S6":0,"S7":0,"S8":0,"S9":0,"S10":0,"S11":0,"S12":0,"S13":0,"S14":0,"S15":0,"S16":0,"S17":0,"S18":0,"S19":0,"S20":0,"S21":0,"S22":0,"S23":0,"S24":0,"S25":0}}}';
+
+
+var json = JSON.parse(data);
+
+// Build answer object
+var answer = {
+    answer01:respondent.answer01,
+    answer02:respondent.answer02,
+    answer03:respondent.answer03,
+    answer04:respondent.answer04,
+    answer05:respondent.answer05
+};
+
+
+
+
+function getPlanValues(json, answer, plan) {
+    var planValue = 0;
+
+
+    planValue = json['Answer01'][answer.answer01][plan] + json['Answer02'][answer.answer02][plan] + json['Answer03'][answer.answer03][plan] + json['Answer04'][answer.answer04][plan];
+
+    //answer 5
+    var answer5Str = answer.answer05
+    var answer5Arr = answer5Str.split(',');
+    var answer5Val = 0;
+    for( var i=0; i<answer5Arr.length; i++){
+        newAnswerVal = answer5Arr[i];
+        answer5Val += json['Answer05'][newAnswerVal][plan];
+    }
+
+	return planValue + answer5Val;
+}
+  if (answer.answer01 == null) {
+    answer.answer01 = "Z"
+  }
+  if (answer.answer02 == null) {
+    answer.answer02 = "Z"
+  }
+  if (answer.answer03 == null) {
+    answer.answer03 = "Z"
+  }
+  if (answer.answer04 == null) {
+    answer.answer04 = "Z"
+  }
+  if (answer.answer05 == null) {
+    answer.answer05 = "Z"
+  }
+
+
+// get plan values
+var S1 = getPlanValues(json, answer, 'S1');
+var S2 = getPlanValues(json, answer, 'S2');
+var S3 = getPlanValues(json, answer, 'S3');
+var S4 = getPlanValues(json, answer, 'S4');
+var S5 = getPlanValues(json, answer, 'S5');
+var S6 = getPlanValues(json, answer, 'S6');
+var S7 = getPlanValues(json, answer, 'S7');
+var S8 = getPlanValues(json, answer, 'S8');
+var S9 = getPlanValues(json, answer, 'S9');
+var S10 = getPlanValues(json, answer, 'S10');
+var S11 = getPlanValues(json, answer, 'S11');
+var S12 = getPlanValues(json, answer, 'S12');
+var S13 = getPlanValues(json, answer, 'S13');
+var S14 = getPlanValues(json, answer, 'S14');
+var S15 = getPlanValues(json, answer, 'S15');
+var S16 = getPlanValues(json, answer, 'S16');
+var S17 = getPlanValues(json, answer, 'S17');
+var S18 = getPlanValues(json, answer, 'S18');
+var S19 = getPlanValues(json, answer, 'S19');
+var S20 = getPlanValues(json, answer, 'S20');
+var S21 = getPlanValues(json, answer, 'S21');
+var S22 = getPlanValues(json, answer, 'S22');
+var S23 = getPlanValues(json, answer, 'S23');
+var S24 = getPlanValues(json, answer, 'S24');
+var S25 = getPlanValues(json, answer, 'S25');
+
+
+
+// Create plan array of all plan scores
+var planArray = [{
+		key: 'S1',
+		value: S1
+	}, {
+		key: 'S2',
+		value: S2
+	}, {
+		key: 'S3',
+		value: S3
+	}, {
+		key: 'S4',
+		value: S4
+	}, {
+		key: 'S5',
+		value: S5
+	}, {
+		key: 'S6',
+		value: S6
+	}, {
+		key: 'S7',
+		value: S7
+	}, {
+		key: 'S8',
+		value: S8
+	}, {
+		key: 'S9',
+		value: S9
+	}, {
+		key: 'S10',
+		value: S10
+	}, {
+		key: 'S11',
+		value: S11
+	}, {
+		key: 'S12',
+		value: S12
+	}, {
+		key: 'S13',
+		value: S13
+	}, {
+		key: 'S14',
+		value: S14
+	}, {
+		key: 'S15',
+		value: S15
+	}, {
+		key: 'S16',
+		value: S16
+	}, {
+		key: 'S17',
+		value: S17
+	}, {
+		key: 'S18',
+		value: S18
+	}, {
+		key: 'S19',
+		value: S19
+	}, {
+		key: 'S20',
+		value: S20
+	}, {
+		key: 'S21',
+		value: S21
+	}, {
+		key: 'S22',
+		value: S22
+	}, {
+		key: 'S23',
+		value: S23
+	}, {
+		key: 'S24',
+		value: S24
+	}, {
+		key: 'S25',
+		value: S25
+	}
+];
+
+
+// Sort plan array alphanumerically
+
+
+planArray.sort(function(a, b){
+    if(a.value === b.value)
+    {
+        var x = a.key, y = b.key;
+        
+        return x < y ? -1 : x > y ? 1 : 0;
+    }
+    return b.value - a.value;
+});
+console.log(planArray);
+
+
+  // Save the Top Plans to Ion Respondent Data
+  actionSaveDataBulk({
+  	"Plan01": planArray[0].key,
+  	"Plan02": planArray[1].key
+  });
+
+
+  // get top plan names from lookup table
+  actionSaveDataBulk({
+  	"Subscription01Name": actionLookupValue("sae_mobilus_subs", planArray[0].key),
+  	"Subscription02Name": actionLookupValue("sae_mobilus_subs", planArray[1].key)
+  });
+
+
+  // get top plan descriptions from lookup table
+  actionSaveDataBulk({
+  	"Subscription01Description": actionLookupValue("sae_mobulus_subsdescription", planArray[0].key),
+  	"Subscription02Description": actionLookupValue("sae-mobilus_subsdescription", planArray[1].key)
+  });
+  
+  actionSaveDataBulk({
+    "S1": S1,
+    "S2": S2,
+    "S3": S3,
+    "S4": S4,
+    "S5": S5,
+    "S6": S6,
+    "S7": S7,
+    "S8": S8,
+    "S9": S9,
+    "S10": S10,
+    "S11": S11,
+    "S12": S12,
+    "S13": S13,
+    "S14": S14,
+    "S15": S15,
+    "S16": S16,
+    "S17": S17,
+    "S18": S18,
+    "S19": S19,
+    "S20": S20,
+    "S21": S21,
+    "S22": S22,
+    "S23": S23,
+    "S24": S24,
+    "S25": S25
+  });
